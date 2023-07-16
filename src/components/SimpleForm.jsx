7fe1efee84a72d7f358c7a1c700976a3398a9c45
sreey <sreelakshmi12898@gmail.com>
2023-07-16 22:15:56 +0530

    Accept input from users

--- src/components/SimpleForm.jsx
+++ src/components/SimpleForm.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 function SimpleForm(){
 
+    const [enteredName, setEnteredName] = useState('');
+
+    const getName = (event) => {
+        setEnteredName(event.target.value);
+    }
+
+    const formSubmit = (event) => {
+        event.preventDefault();
+
+        console.log(enteredName);
+        setEnteredName('')
+    }
+
     return(
-        <form >
+        <form onSubmit={formSubmit} >
             <div className="form-control">
                 <label htmlFor="name">Your Name</label>
-                <input type="text" id="name" />
+                <input type="text" id="name" onChange={getName} />
             </div>
             <div className="form-action">
                 <button>Submit</button>
