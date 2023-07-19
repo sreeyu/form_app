52664564bcea5b6c65fbb7201dbc825334a827c9
sreey <sreelakshmi12898@gmail.com>
2023-07-19 21:59:00 +0530

    Accept inputs from users

--- src/components/BasicForm.jsx
+++ src/components/BasicForm.jsx
@@ -1,22 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
 function BasicForm(){
 
+  const [enteredFName, setEnteredFName] = useState('');
+  const [enteredLName, setEnteredLName] = useState('');
+  const [enteredEmail, setEnteredEmail] = useState('');
+
+  const getFname = (event) => {
+    setEnteredFName(event.target.value);
+  }
+
+  const getLname = (event) => {
+    setEnteredLName(event.target.value);
+  }
+
+  const getEmail = (event) => {
+    setEnteredEmail(event.target.value);
+  }
+
+  const formSubmission = (event) => {
+    event.preventDefault();
+
+    console.log(enteredEmail, enteredFName, enteredLName);
+    setEnteredFName('');
+    setEnteredLName('');
+    setEnteredEmail('');
+  }
+
     return (
-        <form>
+        <form onSubmit={formSubmission}>
           <div className='control-group'>
             <div className='form-control'>
-              <label htmlFor='name'>First Name</label>
-              <input type='text' id='name' />
+              <label htmlFor='fname'>First Name</label>
+              <input value={enteredFName} type='text' id='fname' onChange={getFname} />
             </div>
             <div className='form-control'>
-              <label htmlFor='name'>Last Name</label>
-              <input type='text' id='name' />
+              <label htmlFor='lname'>Last Name</label>
+              <input value={enteredLName} type='text' id='lname' onChange={getLname} />
             </div>
           </div>
           <div className='form-control'>
-            <label htmlFor='name'>E-Mail Address</label>
-            <input type='text' id='name' />
+            <label htmlFor='email'>E-Mail Address</label>
+            <input value={enteredEmail} type='email' id='email' onChange={getEmail} />
           </div>
           <div className='form-actions'>
             <button>Submit</button>
--- src/components/SimpleForm.jsx
+++ src/components/SimpleForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import useInput from "../hooks/use-input";
 
 function SimpleForm(){
